14e82f56b86b8a782b48a3ecef191586a496125c
Florb11 <florbergman11@gmail.com>
2024-09-11 17:42:24 -0300

    primer commit

---
+++ TrabajoIntegrador0/src/Main.java
@@ -0,0 +1,80 @@
+import javax.swing.*;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nombre= validarNombre("Ingrese el nombre del empleado");
+        LocalDate fechaInicio = LocalDate.parse(JOptionPane.showInputDialog("Ingrese la fecha de inicio"));
+        double salarioBase= Double.parseDouble(validarSalario("Ingrese el salario"));
+
+
+        Trabajador trabajador1 = new Trabajador(nombre,fechaInicio,salarioBase);
+        JOptionPane.showMessageDialog(null,trabajador1);
+
+        int vacaciones = trabajador1.calcularDiasVacaciones();
+        JOptionPane.showMessageDialog(null,vacaciones);
+
+        double aumentoSalarial = trabajador1.calcularAumentoSalarial();
+        JOptionPane.showMessageDialog(null,aumentoSalarial);
+
+        double indemnizacion = trabajador1.calcularIndeminizacionDespido(100);
+        JOptionPane.showMessageDialog(null,indemnizacion);
+
+
+
+
+    }
+    public static String validarNombre(String mensaje){
+        boolean flag;
+        String validar;
+
+        do{
+            flag=true;
+            validar=JOptionPane.showInputDialog(null,mensaje);
+            while(validar.isEmpty()){
+                validar=JOptionPane.showInputDialog(null,"Error"+mensaje);
+            }
+            for (int i = 0; i < validar.length(); i++) {
+                if(!Character.isAlphabetic(validar.charAt(i))){
+                    JOptionPane.showMessageDialog(null,"Ingresa el nombre, sin numeros");
+                    flag=false;
+                    break;
+                }
+
+            }
+
+        }while(!flag);
+        return validar;
+    }
+    public static String validarSalario(String mensaje){
+        boolean flag;
+        String validar;
+
+        do{
+            flag=true;
+            validar=JOptionPane.showInputDialog(null,mensaje);
+            while(validar.isEmpty()){
+                validar=JOptionPane.showInputDialog(null,"Error"+mensaje);
+            }
+            for (int i = 0; i < validar.length(); i++) {
+                if(!Character.isDigit(validar.charAt(i))){
+                    JOptionPane.showMessageDialog(null,"Ingresa un salario valido");
+                    flag=false;
+                    break;
+                }
+                double salarioD = Double.parseDouble(validar);
+                if(salarioD <= 0){
+                    JOptionPane.showMessageDialog(null,"Ingresa un salario valido");
+                    flag=false;
+                    break;
+                }
+
+            }
+
+        }while(!flag);
+        return validar;
+    }
+
+
+}
---
+++ TrabajoIntegrador0/src/Trabajador.java
@@ -0,0 +1,77 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Trabajador {
+    //Atributos
+    private String nombre;
+    private LocalDate fechaInicio;
+    private double salarioBase;
+
+    // Constructor
+    public Trabajador(String nombre, LocalDate fechaInicio, double salarioBase){
+        this.nombre=nombre;
+        this.fechaInicio=fechaInicio;
+        this.salarioBase=salarioBase;
+    }
+    // get y set
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+    //metodos
+    public int calcularDiasVacaciones(){
+        int diasVacaciones;
+        LocalDate fechaHoy = LocalDate.now();
+        Period periodo = Period.between(this.fechaInicio,fechaHoy);
+        int anios = periodo.getYears();
+        diasVacaciones = anios*7;
+        return  diasVacaciones;
+
+
+    }
+    public double calcularAumentoSalarial(){
+        int aumento;
+        LocalDate fechaHoy = LocalDate.now();
+        Period periodo = Period.between(this.fechaInicio,fechaHoy);
+        int anios = periodo.getYears();
+        aumento = anios * 1000;
+        return  aumento;
+    }
+    public double calcularIndeminizacionDespido(int salarioBase){
+        double indemnizacion;
+        LocalDate fechaHoy = LocalDate.now();
+        Period periodo = Period.between(this.fechaInicio,fechaHoy);
+        int anios = periodo.getYears();
+        indemnizacion = anios * this.salarioBase;
+        return indemnizacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Trabajador{" +
+                "nombre='" + nombre + '\'' +
+                ", fechaInicio=" + fechaInicio +
+                ", salarioBase=" + salarioBase +
+                '}';
+    }
+}
